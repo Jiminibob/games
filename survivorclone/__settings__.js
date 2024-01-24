29c1c99cf711b30c63b5b3e9873716cf3469df88
Jiminibob <15093657+Jiminibob@users.noreply.github.com>
2024-01-24 14:32:47 +0000

    add survivor clone

---
+++ survivorclone/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1936188.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 164235836, 164235837, 164237409, 164454122, 164456461, 164459135, 164460959, 164461765, 164705419, 164706894, 164707993, 164798070, 164805224 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/164235848/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/164235849/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/164235847/1/ammo.js', 'preload' : true},
+];
